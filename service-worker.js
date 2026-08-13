4fbfc1bed2d11d3d3dcc28d7a625a78b8a3d2f71
whiteringogm <whiteringogm@gmail.com>
2026-08-13 11:43:05 +0900

    Refresh action button cache

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chatapp-v3";
+const CACHE_NAME = "chatapp-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./bubble-actions.js"];
 
 self.addEventListener("install", (event) => {
